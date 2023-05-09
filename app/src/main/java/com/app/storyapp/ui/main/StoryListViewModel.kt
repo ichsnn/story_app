e9f2c3d653a5366d41f3ea49a70000d66ec543f4
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-05-09 08:12:33 +0700

    update StoryListViewModel

--- app/src/main/java/com/app/storyapp/ui/main/StoryListViewModel.kt
+++ app/src/main/java/com/app/storyapp/ui/main/StoryListViewModel.kt
@@ -1,8 +1,7 @@
 package com.app.storyapp.ui.main
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
@@ -13,7 +12,7 @@ import com.app.storyapp.data.remote.response.ListStoryItem
 import com.app.storyapp.data.remote.retrofit.ApiConfig
 import com.app.storyapp.services.StoryPagingSource
 
-class StoryListViewModel(application: Application) : AndroidViewModel(application) {
+class StoryListViewModel : ViewModel() {
 
     fun getStories(token: String): LiveData<PagingData<ListStoryItem>> {
         val pager = Pager(
